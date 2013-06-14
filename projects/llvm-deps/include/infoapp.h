1024185abb57737245c103ea5c8eb11a855dd794
Kangkook Jee <jikk@cs.columbia.edu>
2013-06-13 22:28:43 -0400

    getting there

--- projects/llvm-deps/include/infoapp.h
+++ projects/llvm-deps/include/infoapp.h
@@ -2,30 +2,36 @@
 #define INFOAPP_H_
 
 #include "llvm/Pass.h"
-#include <llvm/Module.h>
+#include "llvm/Module.h"
+
+#include "Infoflow.h"
 
 namespace deps {
 
 using namespace llvm;
 
-class InfoAppPass : public ModulePass {
-  static char ID;
-  InfoAppPass() : ModulePass(ID) {}
-
+class InfoAppPass : public ModulePass {  
   public:
-  bool runOnModule(Module &M) {
-    doInitialization();
-    doFinalization();
-    return false;
+  InfoAppPass() : ModulePass(ID) {}
+  static char ID;
+  bool runOnModule(Module &M);
+
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+    AU.addRequired<Infoflow>();
+    AU.setPreservesAll();
   }
 
   private:
+    Infoflow* infoflow;
     virtual void doInitialization();
     virtual void doFinalization();
 };  // class
 
 /* ID for InfoAppPass */
 char InfoAppPass::ID = 99;
+
+static RegisterPass<InfoAppPass>
+XX ("infoapp", "implements infoapp", true, true);
 
 }  // nameapce
 
--- projects/llvm-deps/lib/InfoApp/infoapp.cpp
+++ projects/llvm-deps/lib/InfoApp/infoapp.cpp
@@ -1,17 +1,29 @@
 #include "llvm/Module.h"
 #include "llvm/Support/Debug.h"
 
-#include "infoapp.h"
+#include "Infoflow.h"
+#include "Slice.h"
+
+#include "Infoapp.h"
 
 namespace deps {
 
 using namespace llvm;
 
 void
-InfoAppPass::doInitialization() {}
+InfoAppPass::doInitialization() {
+  infoflow = &getAnalysis<Infoflow>();
+  DEBUG(errs() << "doInitialization: InfoAppPass\n");
+}
 
 void
 InfoAppPass::doFinalization() {}
 
+bool
+InfoAppPass::runOnModule(Module &M) {
+    doInitialization();
+    doFinalization();
+    return false;
+  }
 
 }  //namespace deps
