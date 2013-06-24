05d0e47f00295acc4df8088f9c0d935861988ed4
Kangkook Jee <jikk@cs.columbia.edu>
2013-06-24 14:55:58 -0400

    roll-back ioc file

--- projects/compiler-rt/lib/ioc/ioc_interface.h
+++ projects/compiler-rt/lib/ioc/ioc_interface.h
@@ -22,8 +22,7 @@
 #endif
 
 #include <stdint.h>
-#include <stdlib.h>
-#include <iconv.h>
+
 
 #define XML_MSG                                     \
   "<structured_message>\n"                          \
@@ -77,14 +76,6 @@ void __ioc_report_conversion(uint32_t line, uint32_t column,
                              const char *srcty, const char *canonsrcty,
                              const char *dstty, const char *canondstty,
                              uint64_t src, uint8_t is_signed);
-
-
-div_t   __ioc_div(int numerator, int denominator);
-ldiv_t  __ioc_ldiv(int numerator, int denominator);
-lldiv_t __ioc_lldiv(int numerator, int denominator);
-size_t __ioc_iconv(iconv_t cd,
-                   char **inbuf, size_t *inbytesleft,
-                   char **outbuf, size_t *outbytesleft);
 
 
 #endif // _IOC_INTERFACE_H_
