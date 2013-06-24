3b9eff8dec08a44ed7b221b702a13ba869ced6d3
Kangkook Jee <jikk@cs.columbia.edu>
2013-06-24 11:55:59 -0400

    update

--- tools/clang/tools/driver/driver.cpp
+++ tools/clang/tools/driver/driver.cpp
@@ -348,12 +348,12 @@ int main(int argc_, const char **argv_) {
                       argv_[1][3] != '1')
       )
   {
-    argv_[argc_++] = "-fioc-unsigned";
-    argv_[argc_++] = "-fioc-signed";
-    argv_[argc_++] = "-fioc-implicit-conversion";
-    argv_[argc_++] = "-fioc-explicit-conversion";
-    argv_[argc_++] = "-fioc-shifts";
-    argv_[argc_++] = "-fioc-strict-shifts";
+//     argv_[argc_++] = "-fioc-unsigned";
+//     argv_[argc_++] = "-fioc-signed";
+//     argv_[argc_++] = "-fioc-implicit-conversion";
+//     argv_[argc_++] = "-fioc-explicit-conversion";
+//     argv_[argc_++] = "-fioc-shifts";
+//     argv_[argc_++] = "-fioc-strict-shifts";
   }
   
   llvm::sys::PrintStackTraceOnErrorSignal();
